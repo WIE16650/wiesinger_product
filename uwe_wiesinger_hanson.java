d2c2c7492d52b86e4c5474ea5c72c0e5401036a1
Uwe Wiesinger <uwe.wiesinger@me.com>
2016-03-29 10:02:51 +0200

    company

---
+++ uwe_wiesinger_hanson.java
@@ -0,0 +1,23 @@
+/**
+ * Autor:   Uwe Wiesinger
+ * Klasse:  4BBIF
+ * Datum:   26.03.2016
+ * Übung:   Git
+ */
+
+
+
+import java.util.*;
+
+public class ArrayListCompany {
+   public static void main(String args[]) {
+      // create an array list
+      ArrayList al = new ArrayList();
+      
+      al.add("Company");
+      
+      // display the array list
+      System.out.println(al);
+      
+   }
+}
